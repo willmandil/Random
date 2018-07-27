769317fb22579a19ee1446ab4b0bf234f162816b
willmandil <willmandil@yahoo.co.uk>
2018-07-27 11:55:39 +0100

    Add files via upload

---
+++ recenter_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+import math
+import rospy
+import numpy as np
+
+def mushroom_avoidance(image)
+    print "here"
+
+def main():
+    heghit = 640
+    width = 480
+    org_img = np.zeros(heght, width, 3)
+
+    mushroom_avoidance(org_img)
+
+
+if __name__ == "__main__":
+    main()
+    # test()
---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import math
+import sys
+
+mushroom_pose = range(0, 360)
+
+mushroom = range(200, 250)
+print mushroom
+
+mushroom1 = range(0, 20)
+mushroom2 = range(340, 359)
+
+mushroom_pose = mushroom_pose.remove[mushroom]
+mushroom_pose = mushroom_pose.remove[mushroom2]
+mushroom_pose = mushroom_pose.remove[mushroom1]
+
+print mushroom_pose
